c8364a2d79c76b5e5a2a91a3f60463103a1aa514
pedrofalc <pedrolfalc@gmail.com>
2018-10-30 16:01:46 -0300

    init repository

---
+++ tl.tcl
@@ -0,0 +1,21 @@
+# set number of nodes
+set opt(nn) 37
+
+# set activity file
+set opt(af) $opt(config-path)
+append opt(af) /activity.tcl
+
+# set mobility file
+set opt(mf) $opt(config-path)
+append opt(mf) /mobility.tcl
+
+# set start/stop time
+set opt(start) 0.0
+set opt(stop) 100.0
+
+# set floor size
+set opt(x) 4703.98
+set opt(y) 5326.84
+set opt(min-x) 89.79
+set opt(min-y) 3157.35
+
